6aa811b0ee2c5abce71ce4367745f93379bf7d6e
SP <sp@SPs-Mac-Pro.local>
2014-09-05 21:20:20 -0700

    added a test class

---
+++ GIT Test/CPTEST.h
@@ -0,0 +1,13 @@
+//
+//  CPTEST.h
+//  GIT Test
+//
+//  Created by SP on 9/5/14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CPTEST : NSObject
+
+@end
---
+++ GIT Test/CPTEST.m
@@ -0,0 +1,13 @@
+//
+//  CPTEST.m
+//  GIT Test
+//
+//  Created by SP on 9/5/14.
+//
+//
+
+#import "CPTEST.h"
+
+@implementation CPTEST
+
+@end
